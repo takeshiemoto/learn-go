e26b7979ca51678ebe33a7d5eb690403f8aad276
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2020-11-15 22:31:22 +0900

    dev

--- struct-example/main.go
+++ struct-example/main.go
@@ -16,10 +16,33 @@ func (t temperature) average() celsius {
 type location struct {
 	lat, long float64
 }
+
+func (l location) days() int {
+	return 5
+}
+
+type sol int
+
+func (s sol) days(s2 sol) int {
+	days := int(s2 - s)
+	if days < 0 {
+		days = -days
+	}
+	return days
+}
+
 type report struct {
-	sol         int
-	temperature temperature
-	location    location
+	sol
+	//temperature temperature
+	temperature
+	location location
+}
+
+// メソッドの衝突回避
+// 上位の型で利用したい方を返す
+// 上位の方が優先される
+func (r report) days(s2 sol) int {
+	return r.sol.days(s2)
 }
 
 func main() {
@@ -29,5 +52,10 @@ func main() {
 	fmt.Printf("%+v\n", report) // {sol:15 temperature:{high:-1 low:-78} location:{lat:-4.5567 long:137.1111}}
 	fmt.Println(report.temperature.high)
 	fmt.Println(t.average())
-	fmt.Println(report.temperature.average())
+	//fmt.Println(report.temperature.average())
+	fmt.Println(report.average()) // temperatureに定義されたメソッドがreportに転送された
+	fmt.Println(report.high)      // メソッドだけでなくフィールドも転送される
+	report.high = 32
+	fmt.Println(report.temperature.high)
+	fmt.Println(report.days(1445))
 }
